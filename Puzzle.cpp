9b4e7d36bb5358cd72add7bb7c4c9e00ec00fbd9
Alex Walhout <alexpwalhout@gmail.com>
2019-09-05 20:28:59 -0700

    Update Puzzle.cpp
    started the cpp

---
+++ Puzzle.cpp
@@ -0,0 +1,12 @@
+#include "Puzzle.h"
+
+Puzzle::Puzzle(std::string code)
+{
+	//parse through code and allocate data to the variables of puzzle
+	//possibly initializing here would prevent needing new memory
+}
+
+void Puzzle::deleteMem()
+{
+	
+}
