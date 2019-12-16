999bbd48ad68e6be3f4dc55f71612d7303fda0f0
Alex Walhout <alexpwalhout@gmail.com>
2019-12-16 13:32:55 -0800

    Update Board.h
    removed unused #include statement

--- Board.h
+++ Board.h
@@ -2,7 +2,6 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include <iostream>
-#include <typeinfo>
 
 //This class stores and manipulates data from an encoded string to construct and display a game board
 class Board
