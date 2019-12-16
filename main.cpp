60f95f991d52aab5dbd40c0f9731825904947bbe
Alex Walhout <alexpwalhout@gmail.com>
2019-12-16 11:58:19 -0800

    cleaned up optional visualization code and main
    removed optional path traversal visualization for the xml file access. cleaned up main's formatting

--- main.cpp
+++ main.cpp
@@ -17,34 +17,11 @@ int main()
 	}
 	else
 		std::cout << "File not found\n";
-	if (!testsResult)
-	{
-		std::cout << "File not found\n";
-	}
 
 	if (testsXML.empty())
-		std::cout << "file is empty\n";
+		std::cout << "File is empty\n";
 	else
-		std::cout << "file is not empty\n";
-
-	//optional traversal visualization
-	std::cout << "Child_value(): " << testsXML.child_value() << std::endl;
-	std::cout << "Child 'trivial': " << testsXML.child("trivial") << std::endl;
-	std::cout << "Child 'trivial' name: " << testsXML.child("trivial").name() << std::endl;
-	std::cout << "Child 'trivial' value: " << testsXML.child("trivial").value() << std::endl;
-	std::cout << "Doc element: " << testsXML.document_element() << std::endl;
-	std::cout << "Doc element value: " << testsXML.document_element().value() << std::endl;
-	//returns "settings"
-	std::cout << "Doc element name: " << testsXML.document_element().name() << std::endl;
-	//returns a non NULL address
-	std::cout << "Doc element last attr: " << testsXML.document_element().last_attribute() << std::endl;
-	std::cout << "Doc element attr 'description': " << testsXML.document_element().attribute("description") << std::endl;
-	std::cout << "Doc element attr 'description' as string: " << testsXML.document_element().attribute("description").as_string() << std::endl;
-
-	std::cout << "Doc element child 'trivial' child 'a': " << testsXML.document_element().child("trivial").child("a") << std::endl;
-	std::cout << "Doc element by path tests/trivial/a attr description as string: " << testsXML.first_element_by_path("tests/trivial/a").attribute("description").as_string() << std::endl;
-	std::cout << std::endl;
-	std::cout << "Doc element by path tests/trivial/a child value: " << testsXML.first_element_by_path("tests/trivial/a").child_value() << std::endl;
+		std::cout << "File is not empty\n";
 
 	//grab the first test's structure string
 	std::string testAStr = testsXML.first_element_by_path("tests/trivial/a").child_value();
@@ -54,36 +31,28 @@ int main()
 	std::string testEStr = testsXML.first_element_by_path("tests/trivial/e").child_value();
 	std::string testFStr = testsXML.first_element_by_path("tests/trivial/f").child_value();
 
-
-
 	//test clusters
 	std::cout << "\n testAStr: " << testAStr << std::endl;
-
 	Puzzle testA(testAStr);
 	testA.printBoard();
 
 	std::cout << "\n testBStr: " << testBStr << std::endl;
-
 	Puzzle testB(testBStr);
 	testB.printBoard();
 
 	std::cout << "\n testCStr: " << testCStr << std::endl;
-
 	Puzzle testC(testCStr);
 	testC.printBoard();
 
 	std::cout << "\n testDStr: " << testDStr << std::endl;
-
 	Puzzle testD(testDStr);
 	testD.printBoard();
 
 	std::cout << "\n testEStr: " << testEStr << std::endl;
-
 	Puzzle testE(testEStr);
 	testE.printBoard();
 
 	std::cout << "\n testFStr: " << testFStr << std::endl;
-
 	Puzzle testF(testFStr);
 	testF.printBoard();
 
